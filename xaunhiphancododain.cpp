55ab66585689567904af61ab5fcc89dbfe9a17e4
jager2002 <94504363+jager2002@users.noreply.github.com>
2021-12-12 10:22:59 +0700

    Add files via upload

---
+++ xaunhiphancododain.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+int n,a[10000],ok;
+void ktao()
+{
+    for(int i=1;i<=n;i++) a[i]=0;
+}
+void sinh()
+{
+    int i=n;
+    while(i!=0 && a[i]==1)
+    {
+        a[i]=0;
+        i--;
+    }
+    if(i==0)
+    {
+        ok=0;
+    }
+    else a[i]=1;
+}
+int main()
+{
+    cin >> n;
+    ktao();
+    ok=1;
+    while(ok)
+    {
+        for(int i=1;i<=n;i++) cout << a[i]<< " ";
+        cout << endl;
+        sinh();
+    }
+}
